37a161ec8440bb41da2111d0c54ac0dac0adc849
KavithFernando <153263149+KavithFernando@users.noreply.github.com>
2024-05-17 17:24:42 +0530

    Dark to Light theme

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,8 @@ if (document.readyState == "loading") {
 }
 
 function ready() {
-    
+
+    // ------------------------------------------------------------------------------------
     const titles = Array.from(document.getElementsByClassName("tab-title"));
     const tabs = Array.from(document.getElementsByClassName("tab-content"));
 
@@ -21,5 +22,38 @@ function ready() {
             document.getElementById(this.id + "-tab").classList.add("active-content");
         });
     });
+    // ------------------------------------------------------------------------------------
+    
+    // ------------------------------------------------------------------------------------
+    const lightButton = document.getElementById("light");
+    const darkButton = document.getElementById("dark");
+
+    lightButton.addEventListener('click', function() {
+        this.style.display = 'none';
+        darkButton.style.display = 'block';
+
+        document.documentElement.style.setProperty('--primary-color', '#F9F9F9');
+        document.documentElement.style.setProperty('--secondary-color', '#2AD882');
+        document.documentElement.style.setProperty('--primary-text-color', '#191D23');
+        document.documentElement.style.setProperty('--subtext-color', '#38414E');
+        document.documentElement.style.setProperty('--special-subtext-color', '#1D8F56');
+
+        document.documentElement.style.setProperty('--astro-brightness-level', '20%');
+    })
+
+    darkButton.addEventListener('click', function() {
+        this.style.display = 'none';
+        lightButton.style.display = 'block';
+
+        document.documentElement.style.setProperty('--primary-color', '#191D23');
+        document.documentElement.style.setProperty('--secondary-color', '#2AD882');
+        document.documentElement.style.setProperty('--primary-text-color', '#FFFFFF');
+        document.documentElement.style.setProperty('--subtext-color', '#D7D7D7');
+        document.documentElement.style.setProperty('--special-subtext-color', '#24ad68');
+
+        document.documentElement.style.setProperty('--astro-brightness-level', '100%');
+
+    })
+    // ------------------------------------------------------------------------------------
 
 }
